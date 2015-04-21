c614bda961b67eea41975f7f2387970af3d01af7
Giovanni Foiani <giovanni.foiani@gmail.com>
2015-04-21 11:58:16 +0200

    Passing tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'main test' do
 
   it "validates true" do
-    expect(true).to eq(false)
+    expect(true).to eq(true)
   end
 
   it "validates false" do
-    expect(false).to eq(true)
+    expect(false).to eq(false)
   end
 
 end
